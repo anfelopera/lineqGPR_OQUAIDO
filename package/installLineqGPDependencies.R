4d4fc5810ee1907dc6fc95185ea5d705693b4ff2
Andres Felipe López-Lopera <anfelopera@users.noreply.github.com>
2019-11-25 13:40:51 +0100

    adding lineqGPR docs

---
+++ package/installLineqGPDependencies.R
@@ -0,0 +1,9 @@
+#### installing dependencies ####
+install.packages(c("stats", "nloptr", "broom", "tmg", "mvtnorm", "purrr"))
+
+#### installing additional dependencies ####
+install.packages(c("MASS", "quadprog", "Matrix", "TruncatedNormal", "DiceDesign",
+                 "graphics", "grDevices", "ggplot2", "plot3D", "profExtrema"))
+
+#### installing additional packages ####                 
+install.packages(c("Rcpp", "testthat", "viridis"))
